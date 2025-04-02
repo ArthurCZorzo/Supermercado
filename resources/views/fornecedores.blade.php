06ce04485aa8f81606fd72c45d178756ce0024e5
Arthur Zorzo <arthurcorteszorzo@gmail.com>
2025-04-02 19:27:04 -0300

    Resolve erro de modal na tela de fornecedor

--- resources/views/fornecedores.blade.php
+++ resources/views/fornecedores.blade.php
@@ -26,28 +26,28 @@
                     </button>
                     </td>
                 </tr>
+
+                <div class="modal fade" id="modalExcluir" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                    <div class="modal-dialog">
+                        <div class="modal-content">
+                        <div class="modal-header">
+                            <h1 class="modal-title fs-5" id="exampleModalLabel">Excluir</h1>
+                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                        </div>
+                        <div class="modal-body">
+                            Deseja excluir o fornecedor {{ $fornecedor->id }} - {{ $fornecedor->nome }}?
+                        </div>
+                        <div class="modal-footer">
+                            <a href="{{ route('fornecedor.excluir', ['id' => $fornecedor->id]) }}" class="btn btn-outline-danger">
+                                Excluir
+                            </a>
+                            <button type="button" data-bs-dismiss="modal" class="btn btn-secondary">Cancelar</button>
+                        </div>
+                        </div>
+                    </div>
+                </div>
             @endforeach
         </tbody>
     </table>
     <a href="{{ route('fornecedor.cadastrar') }}" class="btn btn-primary">Cadastrar Fornecedor</a>
-
-    <div class="modal fade" id="modalExcluir" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-        <div class="modal-dialog">
-            <div class="modal-content">
-            <div class="modal-header">
-                <h1 class="modal-title fs-5" id="exampleModalLabel">Excluir</h1>
-                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-            </div>
-            <div class="modal-body">
-                Deseja excluir o fornecedor {{ $fornecedor->id }} - {{ $fornecedor->nome }}?
-            </div>
-            <div class="modal-footer">
-                <a href="{{ route('fornecedor.excluir', ['id' => $fornecedor->id]) }}" class="btn btn-outline-danger">
-                    Excluir
-                </a>
-                <button type="button" data-bs-dismiss="modal" class="btn btn-secondary">Cancelar</button>
-            </div>
-            </div>
-        </div>
-    </div>
 @endsection
