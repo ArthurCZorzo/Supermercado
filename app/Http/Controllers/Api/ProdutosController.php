e487feec1337870c4ef4dc2be9b9aff8a8b3d4d7
Cristiano Garcia <cristianooo@gmail.com>
2025-04-30 22:02:27 -0300

    _

---
+++ app/Http/Controllers/Api/ProdutosController.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+use App\Models\Produto;
+
+class ProdutosController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        return Produto::all();
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $produto = new Produto();
+        $produto->nome = $request->nome;
+        $produto->preco = $request->preco;
+        $produto->fornecedor_id = $request->fornecedor_id;
+        $produto->foto = $request->foto;
+        #dd($request);
+        $produto->save();
+        return $produto;
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        return Produto::with('fornecedor')
+            ->findOrFail($id);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        $produto = Produto::findOrFail($id);
+        $produto->nome = $request->nome;
+        $produto->preco = $request->preco;
+        $produto->fornecedor_id = $request->fornecedor_id;
+        $produto->foto = $request->foto;
+        
+        $produto->save();
+        return $produto;
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        $produto = Produto::findOrFail($id);
+        $produto->delete();
+
+        return response()
+            ->json(["mensagem"=>"Recurso excluído"], 200);
+        #return response()->noContent();
+    }
+}
